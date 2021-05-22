dd94499dc008a84fdd8c722b49442c3207257c1d
dinaelhusseiny <dinahusseiny15@gmail.com>
2021-05-22 10:32:10 +0200

    Edits in policeStation.py

--- policeStation.py
+++ policeStation.py
@@ -1,7 +1,4 @@
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium import webdriver
 import pandas as pd
 import time
